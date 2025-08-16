1e08ebdab67ddd3508e16b670b9429fe737be546
daviespr1406 <david.espinosa-r@mail.escuelaing.edu.co>
2025-08-16 09:50:28 -0500

    completed the first point with thread excecuted with run and start

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,9 +12,9 @@ public class CountThreadsMain {
         CountThread t2 = new CountThread(100, 199);
         CountThread t3 = new CountThread(200, 299);
 
-        t1.run();
-        t2.run();
-        t3.run();
+        t1.start();
+        t2.start();
+        t3.start();
     }
     
 }
